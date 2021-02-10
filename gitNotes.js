9191b1b584932d8331044fc3bdad51085ef4d1d5
Fandi Ario <fandi.ario10@gmail.com>
2021-02-10 11:37:19 +0700

    Add notes update

---
+++ gitNotes.js
@@ -0,0 +1,24 @@
+// Pertama kali dilakukan
+// 1. Buat new repository di webpage github
+// 2. Initiate Folder : git init
+// 3. Pilih file yg akan ditrack : 
+//  - git add namaFile.ext
+//  - git add .
+// 4. git commit -m "(Your Comment)"
+// 5. git add remote origin link_repo
+// 6. git push origin master
+
+// ----------------------------------
+// git add . 
+// git commit -m " (Comment)"
+// git push origin master
+
+// ---------------------------------
+// git clone (repository)
+// git checkout -b (yourbranch)
+// git add .
+// git commit -m "(Comment)"
+// git push origin (yourbranch)
+// --------------------------------
+// if already approved
+// git push origin master
